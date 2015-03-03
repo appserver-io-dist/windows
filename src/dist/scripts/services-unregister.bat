f91290e6d43e3cab269e134467203ac657e40536
wagnert <t.wagner@techdivision.com>
2015-03-03 11:20:55 +0100

    Add check for already existing services -> restart after installation.

--- src/dist/scripts/services-unregister.bat
+++ src/dist/scripts/services-unregister.bat
@@ -1,16 +1,45 @@
 @ECHO OFF
+
 ECHO Now unregister Services for appserver.io...
 
+REM Change to the installation directory
 cd "$INSTALL_PATH"
 
+REM Initialize the variable with the NSSM binary 
 set nssm=php\nssm.exe
 
-%nssm% stop appserver
-%nssm% remove appserver confirm
+SC QUERY appserver > NUL
 
-ECHO Successfully unregistered Service appserver
+IF ERRORLEVEL 1060 GOTO MISSING
+    
+    REM Stop + Remvoe the Service appserver
+    %nssm% stop appserver
+    %nssm% remove appserver confirm
 
-%nssm% stop appserver-php5-fpm
-%nssm% remove appserver-php5-fpm confirm
+    ECHO Service appserver has successfully been removed
+    
+GOTO END
 
-ECHO Successfully unregistered Service appserver-php5-fpm
+:MISSING
+
+    REM Write a message that the Service appserver has not been registered
+    ECHO Successfully unregistered Service appserver
+
+:END
+
+IF ERRORLEVEL 1060 GOTO MISSING
+    
+    REM Stop + Remvoe the Service appserver-php5-fpm
+    %nssm% stop appserver-php5-fpm
+    %nssm% remove appserver-php5-fpm confirm
+
+    ECHO Service appserver-php5-fpm has successfully been removed
+    
+GOTO END
+
+:MISSING
+
+    REM Write a message that the Service appserver-php5-fpm has not been registered
+    ECHO Successfully unregistered Service appserver-php5-fpm
+
+:END
